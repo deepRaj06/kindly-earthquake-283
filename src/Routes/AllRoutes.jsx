02192d1551b7bb16e39f24ed805be689c045a541
unknown <inder39811@gmail.com>
2022-08-24 08:51:51 +0530

    added carousal component structure

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -7,12 +7,13 @@ import Checkout from "../Pages/Checkout/Checkout";
 import Login from "../Pages/Login/Login";
 import Signup from "../Pages/Signup/Signup";
 import Error from "../Pages/Error/Error";
-
+import ProductListByCity from "../Pages/Products/ProductListByCity";
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/products" element={<ProductList />} />
+      <Route path="/products/:sortBy" element={<ProductListByCity />} />
       <Route path="/products/:id" element={<SingleProduct />} />
       <Route path="/checkout" element={<Checkout />} />
       <Route path="/login" element={<Login />} />
