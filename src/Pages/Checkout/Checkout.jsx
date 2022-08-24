00b97cfced70b981f817c70b575a52cf056fee26
Mohammad Javed <101566706+MJaved9@users.noreply.github.com>
2022-08-24 16:10:14 +0530

    c

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react'
 
 const Checkout = () => {
@@ -7,3 +8,32 @@ const Checkout = () => {
 }
 
 export default Checkout;
+=======
+import { Box, Flex, Text, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import React from "react";
+import style from'./chekout.module.css'
+
+const Checkout = () => {
+  return (
+    <div>
+      <div className="main">
+        <div>
+          <div className={style.one}>
+          <div className="card text-white bg-primary mb-3" style="max-width: 18rem;">
+  <div className="card-header">Header</div>
+  <div className="card-body">
+    <h5 className="card-title">Primary card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+          </div>
+          <div className="two"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default Checkout;
+>>>>>>> Stashed changes
