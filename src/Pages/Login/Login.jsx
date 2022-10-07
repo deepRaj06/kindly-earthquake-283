7ecb92ec727a5af99e82bd2cf3aa3d97f254e669
Deepak <deepak.onlinework13@gmail.com>
2022-10-07 11:46:16 +0530

    api changes in Login.jsx and action.js in AuthReducer

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
 
 
   const loginHandle=()=>{
-
+    console.log(storedData)
 
     // console.log("beore",storedData)
     let fname="";
--- src/Redux/AuthReducer/action.js
+++ src/Redux/AuthReducer/action.js
@@ -4,7 +4,8 @@ import axios from 'axios'
 
 const registerData=(payload)=>(dispatch)=>{
     dispatch({type:types.GET_SIGNUP_REQUEST})
-    axios.get(`http://localhost:8000/signupData`).then((r)=>{
+    // axios.get(`http://localhost:8000/signupData`).then((r)=>{
+    axios.get(`https://api-0231.herokuapp.com/signupData`).then((r)=>{
         dispatch({type:types.GET_SIGNUP_SUCCESS,payload: r.data})
     })
     .catch((e)=>{
@@ -14,7 +15,8 @@ const registerData=(payload)=>(dispatch)=>{
 
 const postData=(payload)=>(dispatch)=>{
     dispatch({type:types.POST_SIGNUP_REQUEST})
-    axios.post(`http://localhost:8000/signupData`,payload).then((r)=>{
+    // axios.post(`http://localhost:8000/signupData`,payload).then((r)=>{
+    axios.post(`https://api-0231.herokuapp.com/signupData`,payload).then((r)=>{
         dispatch({type:types.POST_SIGNUP_SUCCESS,payload: r.data}).then(()=>{
             // console.log("act",r.data)
         })
